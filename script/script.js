ac1ca352ffcd6337e4aed9be7511c9d4e04a1161
Santeri Joki <santeri.joki@gmail.com>
2020-09-23 17:37:37 +0300

    Updated script.js
    Added some style

--- script/script.js
+++ script/script.js
@@ -1,1 +1,22 @@
-//very nice scripts here
+loginblock {
+	margin: 200px 200px 200px 200px;
+}
+
+.clearbutt {
+	padding: 0px;
+	border: none;
+	background-color: inherit;
+	color: black;
+}
+
+.maincontent {
+	padding: 0px;
+	margin: 0px;
+	color: white;
+}
+
+.bottomnav {
+  overflow: hidden;
+  position: fixed;
+  bottom: 0;
+}
